9c9106aaf4f933f75d2a8a57708c07413aadc5bd
GD <gd@wmc.io>
2025-07-14 13:16:02 -0400

    Fix: make keymapp work with Voyager keyboard

--- nixosModules/utilities.nix
+++ nixosModules/utilities.nix
@@ -41,10 +41,12 @@
   services.udev.enable = true;
   services.udev.extraRules = ''
     # Rules for Oryx web flashing and live training
-    # KERNEL=="hidraw*", ATTRS{idVendor}=="16c0", MODE="0664", GROUP="plugdev"
-    # KERNEL=="hidraw*", ATTRS{idVendor}=="3297", MODE="0664", GROUP="plugdev"
-    KERNEL=="hidraw*", ATTRS{idVendor}=="16c0", MODE="0664", TAG+="uaccess"
-    KERNEL=="hidraw*", ATTRS{idVendor}=="3297", MODE="0664", TAG+="uaccess"
+    KERNEL=="hidraw*", ATTRS{idVendor}=="16c0", MODE="0664", GROUP="plugdev"
+    KERNEL=="hidraw*", ATTRS{idVendor}=="3297", MODE="0664", GROUP="plugdev"
+
+    # Keymapp Flashing rules for the Voyager
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="3297", MODE:="0666", SYMLINK+="ignition_dfu"
+
   '';
 }
 
