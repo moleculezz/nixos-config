ef7805bdf6b78975b8aba5fa96dbcd964be293bf
GD <gd@wmc.io>
2025-09-11 14:16:14 -0400

    Add hp drivers

--- hosts/gnix/configuration.nix
+++ hosts/gnix/configuration.nix
@@ -117,6 +117,7 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.printing.drivers = [ pkgs.hplip ];
   services.avahi = {
     enable = true;
     nssmdns4 = true;
